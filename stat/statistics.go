98e958f8731c75e1d5b707d2ec3f428f0aac34a7
shaalx <wisdomswing@gmail.com>
2015-01-09 16:29:49 +0800

    reg

--- stat/statistics.go
+++ stat/statistics.go
@@ -3,6 +3,7 @@ package stat
 import (
 	"fmt"
 	"github.com/shaalx/topic/seg"
+	"regexp"
 	"sort"
 	"strings"
 )
@@ -17,7 +18,10 @@ type Cell struct {
 type CellSlice []*Cell
 
 var sentence string
-var threshold = int32(3)
+var threshold = int32(2)
+var filter []string = []string{
+	"的", "在", "和", "了", "也", "上", "还", "是", "年", "有", "，", "。", " ", "都", "而", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+}
 
 func init() {
 	seg.SetDictionary("dict.txt") // 设定字典
@@ -36,13 +40,13 @@ func Stating() (Stats, []string) {
 func (s Stats) Map2Slice() CellSlice {
 	cellSlice := make(CellSlice, 0)
 	for k, v := range s {
-		if "，" == k {
-			continue
+		filtered := false
+		for _, it := range filter {
+			if it == k {
+				filtered = true
+			}
 		}
-		if "。" == k {
-			continue
-		}
-		if "　" == k {
+		if filtered {
 			continue
 		}
 		if "\n\t" == k {
@@ -72,13 +76,16 @@ func (c CellSlice) Swap(i, j int) {
 
 func (c CellSlice) String() {
 	for i, v := range c {
+		if threshold >= v.Freq {
+			continue
+		}
 		fmt.Println(i, v)
 	}
 }
 
 func FirstStep() {
 	freq, segs := Stating()
-	fmt.Println(freq, segs)
+	// fmt.Println(freq, segs)
 	cells := freq.Map2Slice()
 	sort.Sort(sort.Reverse(cells))
 	cells.String()
@@ -87,16 +94,55 @@ func FirstStep() {
 
 func Search(cells CellSlice, segs []string) {
 	flags := make([]int, 0)
+	// flagsend := make([]int, 0)
+	senbs := []byte(sentence)
 	for _, v := range cells {
 		if v.Freq < threshold {
 			break
 		}
-		index := strings.Index(sentence, v.Word)
-		flags = append(flags, index)
+		// index := strings.Index(sentence, v.Word)
+		// flags = append(flags, index)
+		rege := regexp.MustCompile(v.Word)
+		indexs := rege.FindAllIndex(senbs, -1)
+		for _, it := range indexs {
+			flags = append(flags, it[0])
+		}
 	}
+
+	// // 标点句号 结尾
+	// regend := regexp.MustCompile("。")
+	// indexsend := regend.FindAllIndex(senbs, -1)
+	// for _, it := range indexsend {
+	// 	flagsend = append(flagsend, it[0])
+	// }
+	result := make([]string, 0)
+
 	sort.Ints(flags)
-	fmt.Println(flags)
-	for i := 0; i < len(flags)-1; i += 2 {
-		fmt.Println(segs[flags[i]:flags[i+1]])
+	// fmt.Println(flags)
+	for i := 0; i < len(flags); i++ {
+		// fmt.Println(segs[flags[i]:flags[i+1]])
+		// 查找行首以 H 开头，以。结尾的字符串
+		reg := regexp.MustCompile(`^.*。`)
+		ins := reg.FindAllString(sentence[flags[i]:], -1)
+		for _, in := range ins {
+			// fmt.Printf("%v\n", in)
+			if resContains(result, in) {
+				continue
+			}
+			if strings.Count(in, "。") > 3 {
+				continue
+			}
+			result = append(result, in)
+		}
 	}
+	fmt.Printf("%v\n", result)
+}
+
+func resContains(result []string, in string) bool {
+	for _, it := range result {
+		if strings.Contains(it, in) {
+			return true
+		}
+	}
+	return false
 }
