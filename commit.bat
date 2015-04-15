0f1591be02f881a0f82412afc7e468bb9a621201
shaalx <wisdomswing@gmail.com>
2015-04-15 20:39:20 +0800

    intern

--- commit.bat
+++ commit.bat
@@ -3,8 +3,8 @@ git status
 ::echo commit for change : 
 set /p commit_for_change=
 git commit -m "%commit_for_change%"
-git push oschina master
-git push github master
+git push oschina intern:intern
+git push github intern:intern
 ::net stop server
 ping /n 8 127.1 >nul
 ::net start server
